126b12b4496eedb585f0648276eec9e20ad68724
Kevin Yu <kyu@cvent.com>
2021-12-13 10:02:18 -0800

    feat: time counter

--- 2021/day13/solution.py
+++ 2021/day13/solution.py
@@ -2,12 +2,14 @@ import sys
 
 from aoc import utils
 lines = utils.read_puzzle(__file__)
-height = len(lines)
-for i in range(height):
-    lines[i] = lines[i].split(",")
-lines = utils.int_lines(lines)
 # foldes = [["y", 7], ["x", 5]]
 foldes = [["x", 655], ["y", 447], ["x", 327], ["y", 223], ["x", 163], ["y", 111], ["x", 81], ["y", 55], ["x", 40], ["y", 27], ["y", 13], ["y", 6]]
+
+start_time = utils.time_check()
+height = len(lines)
+for i in range(height):
+    ps = lines[i].split(",")
+    lines[i] = [int(x) for x in ps]
 
 def fold_page(fold, fold_x = False):
     for i in range(0, height):
@@ -40,8 +42,8 @@ def part1():
     for fold in foldes:
         cnt += 1
         dots += fold_page(fold[1], fold[0] == "x")
-        print(f"dots after {cnt} fold: {dots}")
-    print(f"Part1 dots: {dots}")
+        return f"dots after {cnt} fold: {dots}"
+    # print(f"Part1 dots: {dots}")
 
 def part2():
     dots = 0
@@ -51,7 +53,6 @@ def part2():
         dots += fold_page(fold[1], fold[0] == "x")
     
     max_x, max_y = max_xy()
-    print("Result of part2:")
     for y in range(max_y+1):
         for x in range(max_x+1):
             if [x, y] in lines:
@@ -59,6 +60,17 @@ def part2():
             else:
                 print('.', end="")
         print("")
+    return ""
 
-if sys.argv[1]:
-	globals()[sys.argv[1]]()
+if __name__ == "__main__":
+    result = None
+    if sys.argv[1] == "part1":
+        result = part1()
+    elif sys.argv[1] == "part2":
+        result = part2()
+    else:
+        print("Sorry, I don't get it, what's your request?\n")
+        sys.exit()
+    perf  = utils.time_check(start_time)
+    print(result)
+    print(f"Used time of {sys.argv[1]}: {perf['s']} seconds {perf['ms']} ms {perf['µs']} µs {perf['ns']} ns.")
--- aoc/utils.py
+++ aoc/utils.py
@@ -1,4 +1,6 @@
 import os
+from time import perf_counter_ns
+
 def read_puzzle(caller, puzzle = "input.txt"):
     pfile = os.path.join(os.path.dirname(os.path.realpath(caller)), puzzle)
     with open(pfile) as file:
@@ -26,3 +28,20 @@ def int_lines(lines):
     for i in range(height):
         lines[i] = [int(x) for x in lines[i]]
     return lines
+
+def time_check(count_from = None):
+    cur_time = perf_counter_ns()
+    if count_from is None:
+        return cur_time
+    d = perf_counter_ns() - count_from
+    seconds = d//(10**9)
+    mils = (d%(10**9))//(10**6)
+    mics = (d%(10**6))//(10**3)  # µs
+    ns = d%(1000)
+    return {
+        "total_in_ns": d,
+        "s": seconds,
+        "ms": mils,
+        "µs": mics,
+        "ns": ns
+    }
