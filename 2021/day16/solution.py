8567e449b5562036bbe5d74f8ea676683b426ae2
Kevin Yu <kyu@cvent.com>
2021-12-15 22:06:31 -0800

    initiate day16

---
+++ 2021/day16/solution.py
@@ -0,0 +1,28 @@
+import sys
+
+from aoc import utils
+lines = utils.read_puzzle(__file__)
+start_time = utils.time_check()
+height = len(lines)
+width = len(lines[0])
+
+def part1():
+    return "result of part1"
+
+def part2():
+    return "result of part2"
+
+if __name__ == "__main__":
+    result = None
+    if len(sys.argv) < 2 or sys.argv[1] not in ["part1", "part2"]:
+        print("\nSorry, I don't get it, \nI need know which part you want to run, part1 or part2?\n")
+        sys.exit()
+    if sys.argv[1] == "part1":
+        result = part1()
+    else :
+        result = part2()
+        
+    perf  = utils.time_check(start_time)
+    print(result)
+    print(f"{sys.argv[1]} time used: {perf['s']} seconds {perf['ms']} ms {perf['µs']} µs {perf['ns']} ns.")
+
