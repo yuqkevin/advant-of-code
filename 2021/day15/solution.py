b709ed9d68a6d846aeb9f12e68ca4b41bd9c9ff6
Kevin Yu <kyu@cvent.com>
2021-12-19 09:25:58 -0800

    working

--- 2021/day15/solution.py
+++ 2021/day15/solution.py
@@ -72,11 +72,99 @@ def find_paths(maze):
     print(f"found {len(paths)} paths")
     return paths
 
+def is_for_pool(maze, pool, x, y):
+    dm = len(maze)
+    val = maze[x][y]
+    if val > 0:
+        return False
+    if (x, y) in pool:
+        return False
+    neighours = [[min(dm - 1, x + 1), y], [x, min(dm - 1, y + 1)], [max(0, x -1), y], [x, max(0 , y - 1)]]
+    for p in neighours:
+        if maze[p[0]][p[1]] < 1 and tuple(p) in pool:
+            return True
+
+def find_pools(maze, a, b):
+    pools = [{tuple(a)}, {tuple(b)}]
+    dm = len(maze)
+    pointers = [[0, 0], [dm - 1, dm - 1]]
+    for x in range(a[0], dm):
+        for y in range(a[0], dm):
+            # print(f"check pt [{x}, {y}] for pool 0")
+            if is_for_pool(maze, pools[0], x, y):
+                pools[0].add((x, y))
+                pointers[0] = [max(pointers[0][0], x), max(pointers[0][1], y)]
+            elif x > pointers[0][0] + 1 and y > pointers[0][1] + 1:
+                break
+    dm_b = b[1] + 1
+    for i in range(dm_b):
+        for j in range(dm_b):
+            x = b[1] - i
+            y = b[1] - j
+            if is_for_pool(maze, pools[1], x, y):
+                pools[1].add((x, y))
+                pointers[1] = [min(pointers[1][0], x), min(pointers[1][1], y)]
+            elif x < pointers[1][0] - 1 and y < pointers[1][1] - 1:
+                break
+    return (pools, pointers)
+    # return  (pools[0].intersection(pools[1]))
+
+def find_point(pools, pointers):
+    pass
+def find_points(maze, a, b):
+    points = []
+    point = None
+    cnt = 0
+    off_pools = []
+    off_pointers = []
+    on_pools = []
+    off_maze = []
+    while cnt < 3 and True:
+        cnt += 1
+        pools, pointers = find_pools(maze, a, b)
+        if len(pools[0]) == 1 and len(pools[1]) == 1:
+            maze = [(lambda x: [ z - 5 for z in x])(y)  for y in maze]
+            continue
+        if len(pools[0].intersection(pools[1])) == 0:
+            off_pools = pools
+            off_pointers = pointers
+            off_maze = maze.copy()
+            if len(on_pools) == 0:
+                maze = [(lambda x: [ z - 1 for z in x])(y)  for y in maze]
+                continue
+        else:
+            on_pools = pools
+            if len(off_pools) == 0:
+                maze = [(lambda x: [ z + 1 for z in x])(y)  for y in maze]
+                continue
+        print(off_pools)
+        print_maze(off_maze)
+        print(off_pointers)
+        # print_maze(maze)
+            
+        
+     
 def test(input = "test-input.txt"):
-    maze = utils.read_puzzle(__file__, input)
-    maze = [(lambda x: [int(z) for z in list(x)])(y)  for y in maze]
-    paths = find_paths(maze.copy())
-    return path_risk(paths, maze)
+    lines = utils.read_puzzle(__file__, input)
+    # dm = len(lines)
+    # a = lines[0][0]
+    # z = lines[dm -1][dm - 1]
+    
+    # pool = []
+    # for line in lines:
+    #     pool += [int(x) for x in line]
+    # pool = [x - 1 for x in pool]
+    # find_pools(pool, a, z)
+    # print(pool)
+    maze = [(lambda x: [int(z) for z in list(x)])(y)  for y in lines]
+    # maze = [(lambda x: [ int(z) - 5 for z in x])(y)  for y in lines]
+    dm = len(maze)
+    find_points(maze, [0, 0], [dm - 1, dm - 1])
+    print("============")
+    print_maze(maze)
+
+    # paths = find_paths(maze.copy())
+    # return path_risk(paths, maze)
 
 def part1():
     maze = utils.read_puzzle(__file__)
